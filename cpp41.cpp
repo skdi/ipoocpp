b2c188cbed95668e3a448dab1a0ad3276f730541
Skdi <jddhghf@gmail.com>
2016-10-23 22:12:55 -0500

    Update cpp41.cpp

--- cpp41.cpp
+++ cpp41.cpp
@@ -18,3 +18,37 @@ cin >> number ;
 //1/1/2/362880/3628800
 //1
 //deberia ser 15511210043330985984000000 y es 2076180480
+
+//continuacion, ediciion para numeros positivos y no muy grandes
+/*
+int main ()
+{
+short number ;
+cout << " Enter a number : ";
+cin >> number ;
+
+ cout << "The factorial of " << number << " is ";
+ int accumulator = 1;
+ if(number>=0 && number<11){
+     for (; number > 0; accumulator *= number --);
+     cout << accumulator << ".\n";
+ }else cout<<"numero invalido"<<endl;
+
+ return 0;
+ }
+ 
+ switch(number){
+ case n==11:
+      cout<<"factorial 11"<<endl;
+ case n==12:
+      cout<<"factorial 12"<<endl;
+ case n==13:
+      cout<<"factorial 13"<<endl;
+ case n==14:
+      cout<<"factorial 14"<<endl;
+ default:
+      cout<<"numero muy grande"<<endl;*/
+
+//deberiamos considerar el caso de numeros reales en general y ponerles una sentencia.
+ 
+ 
